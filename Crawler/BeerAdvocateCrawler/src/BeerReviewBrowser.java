2fd9c68e2aa4a03aa400571af06c41ef5fa959f4
Shwetank <shwetank.raghuvanshi@gmail.com>
2014-11-12 23:50:51 -0500

    Additions to beer advocate crawler

--- Crawler/BeerAdvocateCrawler/src/BeerReviewBrowser.java
+++ Crawler/BeerAdvocateCrawler/src/BeerReviewBrowser.java
@@ -1,7 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.*;
+import java.util.List;
 
 /**
  * Created by maverick on 11/5/14.
@@ -33,5 +36,22 @@ public class BeerReviewBrowser {
 
     private static void processBeer(String url) {
         beerReviewDriver.get(url);
+        WebElement temp = null;
+        List<WebElement> reviews = beerReviewDriver.findElements(By.id("rating_fullview_container"));
+        for(WebElement element : reviews) {
+            element = element.findElement(By.id("rating_fullview_content_2"));
+            temp = element.findElement(By.className("BAscore_norm"));
+            String bascore = temp.getText();
+            temp = element.findElement(By.className("rAvg_norm"));
+            String norm = temp.getText();
+            List<WebElement> mutedClass = element.findElements(By.className("muted"));
+            temp = mutedClass.get(0);
+            String rating = temp.getText();
+            String review = element.getText();
+            temp = mutedClass.get(1);
+            temp = temp.findElement(By.className("username"));
+            String username = temp.getText();
+
+        }
     }
 }
